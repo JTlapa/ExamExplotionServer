754b0ba372397191d518c9a129b79cc74768fad3
Jesus Tlapa <jesus.tlapa11@gmail.com>
2024-10-12 15:19:22 -0600

    Primera parte del chat implementada, falta considerar aspectos de concurrencia y desconecciones esperadas e inesperadas

---
+++ Server/ServerServices/ILobbyManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerService
+{
+    [ServiceContract(CallbackContract = typeof(ILobbyConnectionCallback))]
+    public interface ILobbyManager
+    {
+        [OperationContract(IsOneWay = true)]
+        void SendMessage(string user, string message);
+
+        [OperationContract]
+        bool Connect(string user);
+
+    }
+    public interface ILobbyConnectionCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void ReceiveMessage(string gamertag, string message);
+    }
+}
--- Server/ServerServices/ServiceImplementation.cs
+++ Server/ServerServices/ServiceImplementation.cs
@@ -1,5 +1,8 @@
 ﻿using DataAccess;
 using DataAccess.EntitiesManager;
+using System;
+using System.Collections.Generic;
+using System.ServiceModel;
 
 namespace ServerService
 {
@@ -14,6 +17,32 @@ namespace ServerService
             bool result = AccountManagerDB.ValidateAccount(accountToValidate);
 
             return result;
+        }
+    }
+    public partial class ServiceImplementation : ILobbyManager
+    {
+        private static Dictionary<string, ILobbyConnectionCallback> connectedUsers = new Dictionary<string, ILobbyConnectionCallback>();
+
+        public bool Connect(string gamertag)
+        {
+            var callback = OperationContext.Current.GetCallbackChannel<ILobbyConnectionCallback>();
+
+            if (!connectedUsers.ContainsKey(gamertag))
+            {
+                connectedUsers.Add(gamertag, callback);
+                Console.WriteLine($"{gamertag} conectado.");
+                return true;
+            }
+
+            return false;
+        }
+
+        public void SendMessage(string gamertag, string message)
+        {
+            foreach (var client in connectedUsers.Values)
+            {
+                client.ReceiveMessage(gamertag, message);
+            }
         }
     }
 }
