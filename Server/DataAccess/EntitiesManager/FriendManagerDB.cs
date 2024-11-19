e033746a2c118483c5ba8b2aa3f5eb6a2a657ef9
Jesus Tlapa <jesus.tlapa11@gmail.com>
2024-11-19 14:58:03 -0600

    Cambios tlapa

---
+++ Server/DataAccess/EntitiesManager/FriendManagerDB.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.EntitiesManager
+{
+    public class FriendManagerDB
+    {
+        public static List<int> GetFriendsIdByPlayer(int userId)
+        {
+            List<int> friendsId = new List<int>();
+            using (var context = new ExamExplotionDBEntities())
+            {
+                var friends = context.Friend.Where(f => f.playerId1 == userId).Select(f => f.playerId2).ToList();
+                friendsId = friends;
+            }
+            return friendsId;
+        }
+    }
+}
